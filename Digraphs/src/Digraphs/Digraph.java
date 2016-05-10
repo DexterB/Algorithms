68d386152af6c6b091be70f60f65b4320d7c3e6c
Dexter Bradshaw <dexter.bradshaw@hbo.com>
2016-05-09 21:39:05 -0700

    Added methods to query the number of vertices, the number of edges, and the adjacency list of a vertex.

--- Digraphs/src/Digraphs/Digraph.java
+++ Digraphs/src/Digraphs/Digraph.java
@@ -29,6 +29,32 @@ public class Digraph {
 	}
 	
 	/**
+	 * Query the number of vertices in the digraph.
+	 * @return number of vertices in the digraph.
+	 */
+	public int V() {
+		return V;
+	}
+	
+	/**
+	 * Query the number of edges in the digraph.
+	 * 
+	 * @return number of edges in the digraph.
+	 */
+	public int E() {
+		return E;
+	}
+	
+	/**
+	 * Query the adjacency list for a given vertex.
+	 * @param v is the target vertex for the adjacency list
+	 * @return adjacency list for v.
+	 */
+	public Iterable<Integer> adj(int v) {
+		return adj[v];
+	}
+	
+	/**
 	 * Add a directed edge from v to w into the digraph.
 	 * @param v is the node at the start of the directed edge.
 	 * @param w is the node at the end of the directed edge.
